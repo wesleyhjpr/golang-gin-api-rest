026d643afdd8abc445f9e956d9fabb8ece0ad87f
wesleyhjpr <wesleyhjpr@gmail.com>
2022-02-20 13:52:06 -0300

    first conifg package gin

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetAllStudents(c *gin.Context){
+	c.JSON(200, gin.H {
+		"id":"1",
+		"name":"Wesley",
+	})
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/students", GetAllStudents)
+	
+	r.Run()
+}
